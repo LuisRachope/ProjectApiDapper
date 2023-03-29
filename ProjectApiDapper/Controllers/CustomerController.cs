e7cb3a64b39fd3196d2d2edac811512fc88faf61
Luis Rachope <luis.chiasso@hotmail.com>
2023-03-29 18:02:16 -0300

    Add project files.

---
+++ ProjectApiDapper/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectApiDapper.Models;
+using System.Data.SqlClient;
+
+//https://www.youtube.com/watch?v=n0zkkoL8eNs
+namespace ProjectApiDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public readonly IConfiguration _config;
+
+        public CustomerController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Customer>>> GetAll()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var customers = await connection.QueryAsync<Customer>("select * from customers;");
+            return Ok(customers);
+
+        }
+    }
+}
