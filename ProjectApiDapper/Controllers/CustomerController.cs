3e9883b3cac1ccbf91faf7021fd9314b2ca29338
Luis Rachope <luis.chiasso@hotmail.com>
2023-03-31 20:28:46 -0300

    Criada as operações de delete e get by id

--- ProjectApiDapper/Controllers/CustomerController.cs
+++ ProjectApiDapper/Controllers/CustomerController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjectApiDapper.Models;
+using ProjectApiDapper.Models.Dtos;
 using System.Data.SqlClient;
 
 //https://www.youtube.com/watch?v=n0zkkoL8eNs
@@ -25,6 +26,48 @@ namespace ProjectApiDapper.Controllers
             var customers = await connection.QueryAsync<Customer>("select * from customers;");
             return Ok(customers);
 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetById(int id)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var customer = await connection.QueryFirstAsync<Customer>("select * from customers where Id = @id;",
+                new { Id = id });
+
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Customer>> Create([FromBody] CreateCustomerDto customerDto)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+            await connection.QueryAsync("INSERT INTO customers (Name, FirstName, LastName, Place)" +
+                "VALUES (@Name, @FirstName, @LastName, @Place);", 
+                new { Name = customerDto.Name, FirstName = customerDto.FirstName, LastName = customerDto.LastName, Place = customerDto.Place }
+                );
+
+            return Ok(customerDto);
+        }
+
+        [HttpDelete("id")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+            try
+            {
+                await connection.QueryAsync("DELETE FROM customers WHERE Id = @id;",
+                new { Id = id });
+            }
+            catch (Exception)
+            {
+
+                return NotFound(false);
+            }
+
+            return Ok(true);
         }
     }
 }
---
+++ ProjectApiDapper/Models/Dtos/CreateCustomerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectApiDapper.Models.Dtos
+{
+    public class CreateCustomerDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
